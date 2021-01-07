baa50a64185f1dd5e846bcde1baa2bd77b3fdcb2
Alfonzo <75266027+Alfonzo-dll@users.noreply.github.com>
2021-01-07 17:42:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,4 +21,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run("Nzk2Njk2MTQzMzA3NjAzOTg5.X_brGg.qVnRSdM0rrqgFT5Jemhld0t7ffE")
+client.run("token")
