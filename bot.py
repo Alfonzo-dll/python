46e158da3c9d1ca2b883b25156fb6feec254dced
Alfonzo <75266027+Alfonzo-dll@users.noreply.github.com>
2021-01-07 18:04:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,13 @@ import discord
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '?')
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix = '?', intents=intents)
+
+@client.event
+async def on_ready():
+     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Bot with Python"))
+     print('Bot is online and connected to discord')
 
 @client.command()
 async def load(ctx, extension):
@@ -21,4 +27,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run("token")
+client.run("Nzk2Njk2MTQzMzA3NjAzOTg5.X_brGg.GAMBhWn_0jyS1fUi0BcsVReuA6w")
